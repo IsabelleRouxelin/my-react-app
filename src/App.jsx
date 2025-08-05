85e8e7249de0e032ed81dea15cc39880c3cfed48
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-08-05 20:17:25 +0100

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import { useState } from 'react';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
@@ -6,16 +5,13 @@ import AboutPage from './pages/AboutPage';
 import ProjectPage from './pages/ProjectPage';
 
 function App() {
-
   const [page, setPage] = useState('home');
 
   return (
     <Layout selectedPage={page} onSetPage={setPage}>
-
       {page === 'home' && <HomePage />}
       {page === 'about' && <AboutPage />}
-      {page === 'project' && <ProjectPage />}
-
+      {page === 'projects' && <ProjectPage />}
     </Layout>
   );
 }
