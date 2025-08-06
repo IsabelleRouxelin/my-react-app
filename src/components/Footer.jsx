68fe59357033a6cc1f1a305aa873a5fef9e9b734
IsabelleRouxelin <isabelle.rouxelin@gmail.com>
2025-08-06 01:31:03 +0100

    removed quicklinks from footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import '../styles/main.css';
 // look into adding icons if possible
 const Footer = () => {
   return (
-    <footer className='footer'>
-        <div className='footer-container'>
+    <footer className='footer' id="contact">
+        <div className='container'>
+          <div className='footer-content'>
+            <div className='social-links'>
+              <a href="https://www.linkedin.com/in/irouxelin/" className="social-link">Linkedin</a>
+              <a href="https://github.com/IsabelleRouxelin" className="social-link">Github</a>
+              <a href="mailto:isabelle.rouxelin@gmail.com" className="social-link">Mail</a>
+            </div>
             <h2 className='footer-title'>Let's Connect</h2>
-            <p className='footer-description'>I'm always open to discussing new opportunities & interesting projects.</p>
-            <div>
-                <a href="mailto:isabelle.rouxelin@gmail.com" className='contact-link'></a>
-                <a href="https://github.com/IsabelleRouxelin" className='contact-link'></a>
-                <a href="https://www.linkedin.com/in/irouxelin/" className='contact-link'></a>
+            <p className='footer-'>I'm always open to discussing new opportunities & interesting projects.</p>
+          </div>
+            <div className='copyright'>
+              &copy; {new Date().getFullYear()} Isabelle Rouxelin. All rights reserved.
             </div>
-            <div>
-                <p>&copy; 2025 Isabelle Rouxelin. All rights reserved.</p>
-            </div>
+
         </div>
     </footer>
   )
